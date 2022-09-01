6a936bdcb875b55ebce52ebd3ffc05961416964d
UICHEOL-HWANG <icuchoel@gmail.com>
2022-09-01 23:05:23 +0900

    second

--- EDA.py
+++ EDA.py
@@ -7,3 +7,5 @@ import plotly.figure_factory as py
 import plotly.express as px
 import matplotlib.pyplot as plt # 라이브러리들 호출 
 
+data = pd.read_csv("C:\\Users\\UICHEOL_HWANG\\new_project\\books_1.Best_Books_Ever.csv",usecols=['title', 'series', 'author', 'rating', 'language', 'genres', 'characters', 'pages', 'publishDate', 'awards', 'numRatings', 'likedPercent', 'price'])
+data.head(5) #데이터 프레임 추출 
